ce90a55f95332c006b9879b04a48d7968979260c
sidane17 <khalilsidane89@gmail.com>
2024-04-22 11:10:38 +0200

    ora si

--- js.js
+++ js.js
@@ -1,11 +1,33 @@
-const st=document.querySelector("main.lista");
- for(let i=1;i<=50;i++){
-    const div= '<div id="p" class="box"></div>';
+
+let i=1
+let st=document.getElementById("main");
+ for( i=1;i<=50;i++){
+     if(i%3==0){
+        console.log("tre")
+    st+=`<div class="box tre">buzz</div>`;
+      
+    }else if(i%5==0){
+        console.log("cinque")
+        st+=`<div class="box cinque">fizz</div>`;
+
+    }
+     if(i%5==0 && i%3==0){
+        console.log("2")
+        st+=`<div class="box entrambe">FizzBuzz</div>`;
+
+    }else{
+        st+=`<div class="box ">${i}</div>`;
+    }
     
-    st.innerHTML+=div;
-    div.innerHTML+=i;
+    
+
+    
+   
      console.log(i);
+    
      
  }
+
+document.getElementById("main").innerHTML=st;
 
 
